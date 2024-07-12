b4c2c697e5fe12db4b418f64c691a3758e37dc4f
krishnpal singh chouhan <krishnpalchouhan@gmail.com>
2024-07-12 15:43:55 +0530

    Initial commit

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-test 
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('voice', (data) => {
+    socket.broadcast.emit('voice', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+const port = process.env.PORT || 3000;
+http.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
