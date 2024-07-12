dd39e8e9b188bf5f21d0a409fe4d87a48db4e761
krishnpal singh chouhan <krishnpalchouhan@gmail.com>
2024-07-12 16:23:58 +0530

    build

--- pages/api/socket.ts
+++ pages/api/socket.ts
@@ -8,13 +8,21 @@ const SocketHandler = (req: any, res: any) => {
     const io = new Server(res.socket.server);
     res.socket.server.io = io;
 
-    io.on('connection', socket => {
+    io.on('connection', (socket) => {
+      console.log(`New connection: ${socket.id}`);
+
       socket.on('join-room', (roomId) => {
         socket.join(roomId);
+        console.log(`${socket.id} joined room: ${roomId}`);
       });
 
       socket.on('send-audio', (audioChunk, roomId) => {
         socket.to(roomId).emit('receive-audio', audioChunk);
+        console.log(`Audio chunk sent to room: ${roomId}`);
+      });
+
+      socket.on('disconnect', () => {
+        console.log(`Socket disconnected: ${socket.id}`);
       });
     });
   }
