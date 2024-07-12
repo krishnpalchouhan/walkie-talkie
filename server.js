2e58efdc5c22ab134bfb218b707a9b9d5124191a
krishnpal singh chouhan <krishnpalchouhan@gmail.com>
2024-07-12 15:48:01 +0530

    init

--- server.js
+++ server.js
@@ -5,14 +5,34 @@ const io = require('socket.io')(http);
 
 app.use(express.static('public'));
 
+const channels = {};
+
 io.on('connection', (socket) => {
   console.log('A user connected');
 
+  socket.on('join', (channel) => {
+    socket.join(channel);
+    if (!channels[channel]) {
+      channels[channel] = new Set();
+    }
+    channels[channel].add(socket.id);
+    console.log(`User joined channel: ${channel}`);
+  });
+
   socket.on('voice', (data) => {
-    socket.broadcast.emit('voice', data);
+    const channel = Array.from(socket.rooms)[1]; // Get the channel room
+    if (channel) {
+      socket.to(channel).emit('voice', data);
+    }
   });
 
   socket.on('disconnect', () => {
+    for (const channel in channels) {
+      channels[channel].delete(socket.id);
+      if (channels[channel].size === 0) {
+        delete channels[channel];
+      }
+    }
     console.log('A user disconnected');
   });
 });
